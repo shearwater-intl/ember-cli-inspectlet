5742d1fa80b81ccef31071fe1fd38041dcf86698
Max Wallace <maxfield.wallace@gmail.com>
2015-12-09 12:56:37 -0500

    Update script (not sure if different)

--- index.js
+++ index.js
@@ -8,14 +8,20 @@ module.exports = {
     var wid = config.APP.INSPECTLET_WID;
 
     if (wid != null && type === 'head') {
-      return "<script>\n" +
-        "window.__insp = window.__insp || [];\n" +
-        "__insp.push(['wid', " + wid + "]);\n" +
-        "(function() {\n" +
-        "function ldinsp(){if(typeof window.__inspld != 'undefined') return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = 'inspsync'; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };\n" +
-        " setTimeout(ldinsp, 500); document.readyState != 'complete' ? (window.attachEvent ? window.attachEvent('onload', ldinsp) : window.addEventListener('load', ldinsp, false)) : ldinsp();\n" +
-        "})();" +
-        "</script>"
+
+      return `<script type="text/javascript" id="inspectletjs">
+        window.__insp = window.__insp || [];
+        __insp.push(['wid', ` +
+
+        wid +
+
+        `]);
+        (function() {
+        function ldinsp(){if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+        setTimeout(ldinsp, 500); document.readyState != "complete" ? (window.attachEvent ? window.attachEvent('onload', ldinsp) : window.addEventListener('load', ldinsp, false)) : ldinsp();
+        })();
+      </script>`;
     }
-  }
+  },
+
 };
